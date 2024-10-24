3e4f4545a704a8820092a0e302b90ad5384e0ec0
Seungeun727 <seun2230@gmail.com>
2024-10-24 11:07:32 +0900

    feat: map 연습/응용

--- functiion/arrow/basic_02.js
+++ functiion/arrow/basic_02.js
@@ -3,24 +3,24 @@
 // 목표 : 화살표 함수(Arrow Function)로 복잡도 감소
 
 const name = {
-    first: 'Lemy',
-    last: 'Kilmister',
+	first: 'Lemy',
+	last: 'Kilmister',
 };
 
 
 // 기본 함수
 function getName({first, last}) {
-    return `${first} ${last}`;
+	return `${first} ${last}`;
 }
 
 // 화살표 함수
 // const getName2 = { first, last } => `${first} ${last}`;  // ; expected 
 
 const comic = {
-    first: 'Peter',
-    last: 'Bagge',
-    city: 'Seattle',
-    state: 'Washington'
+	first: 'Peter',
+	last: 'Bagge',
+	city: 'Seattle',
+	state: 'Washington'
 };
 
 const getFullName = ({ first, last }) => `${first} ${last}`;
@@ -28,17 +28,17 @@ getFullName(comic);
 
 
 const getNameAndLocation = ({ first, last, city, state }) => ({
-    fulName: `${first} ${last}`,
-    location: `${city} ${state}`,
+	fulName: `${first} ${last}`,
+	location: `${city} ${state}`,
 });
 getNameAndLocation(comic);
 
 
 // 고차 함수의 반환 값 : 함수 몸체에서 다른 함수를 반환
 const discounter = discount => {
-    return price => {
-        return price * ( 1- discount);
-    };
+	return price => {
+		return price * ( 1- discount);
+	};
 };
 
 const tenPercentOff = discounter(0.1);
@@ -48,3 +48,38 @@ const discounter2 = discount => price => price * (1-discount);
 
 const tenPercentOff2 = discounter(0.1);
 tenPercentOff2(100);
+
+
+const students = [
+	{ id: 0, name: 'Jake'},
+	{ id: 1, name: 'Navi'},
+	{ id: 2, name: 'John'},
+	{ id: 3, name: 'Rain'},
+	{ id: 4, name: 'Nami'},
+];
+
+// 객체 내 속성 접근 
+let personData = students.map((person, index) => person.name);
+console.log(personData);
+
+
+let personList = students.map((student) => {
+	let personObj = {};
+	personObj[student.id] = student.name;
+	return personObj;  
+});
+
+console.log("personList result: ",personList);
+
+let personData2 = students.map((student, index) => {
+	let newArr = [];
+	if(index % 2 == 0) newArr.push(student.name);
+	return newArr;
+},[])
+
+let personData3 = students.map((student, index) => {
+	return Object.assign(student, { grade: 2});
+})
+
+console.log("personData2 result: ", personData2);
+console.log("personData3 result: ", personData3);
